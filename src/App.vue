195ecdc13a9c3e4893a1f96c16e4ebfe5a76d7d3
LIN WAN-TING <bibo9851g23@gmail.com>
2024-03-13 10:00:45 +0800

    add commit in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h1 class="Title">TodoList</h1>
+  <!-- 代辦事項輸入框 -->
   <div class="TodoEnter">
     <input
       v-model="todoEnter"
@@ -8,6 +9,7 @@
     />
     <button @click="addTodo(todoEnter)">Enter</button>
   </div>
+  <!-- 顯示代辦事項 -->
   <div class="TodoList">
     <ol v-if="todos.length">
       <li
@@ -26,6 +28,7 @@
       </li>
     </ol>
   </div>
+  <!-- 分類代辦事項 全部All已完成Done未完成Undone -->
   <div class="TodoFilter">
     <button @click="All">All</button>
     <button @click="Done">Done</button>
@@ -44,27 +47,28 @@ export default {
   },
 
   methods: {
+    // 新增代辦事項
     addTodo(todo) {
       if (todo) {
         this.todos.push({ todoItem: todo, todoCheck: false, show: true });
         this.todoEnter = "";
       }
     },
-
+    // 刪除代辦事項
     deleteTodo(todoIndex) {
       this.todos.splice(todoIndex, 1);
     },
-
+    // 代辦事項確認框
     checkTodo(todoIndex) {
       this.todos[todoIndex].todoCheck = !this.todos[todoIndex].todoCheck;
     },
-
+    // filter-All 全部代辦事項
     All() {
       for (var i = 0; i < this.todos.length; i++) {
         this.todos[i].show = true;
       }
     },
-
+    // filter-Done 已完成代辦事項
     Done() {
       for (var i = 0; i < this.todos.length; i++) {
         if (!this.todos[i].todoCheck) {
@@ -74,7 +78,7 @@ export default {
         }
       }
     },
-
+    // filter-Undone 未完成代辦事項
     Undone() {
       for (var i = 0; i < this.todos.length; i++) {
         if (this.todos[i].todoCheck) {
